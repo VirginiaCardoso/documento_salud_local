8dc1b0d456525250010b5fec43a8cac679894c92
VirginiaCardoso <virgicardoso@gmail.com>
2017-01-09 13:00:38 -0300

    vista muetras

--- views/pool-lab/sin-muestras.php
+++ views/pool-lab/sin-muestras.php
@@ -21,7 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h3 class="panel-title">Confirma la extracción y toma de muestra?  </h3>
+                <h3 class="panel-title">¿ Confirma la extracción y toma de muestra?  </h3>
             </div>
             <div class="panel-body">
                 <div class="row">
@@ -30,40 +30,35 @@ $this->params['breadcrumbs'][] = $this->title;
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-md-2">
-                        <?='<label class=" control-label col-md-4" for="fecha">Fecha</label>' ?>
-                    </div>
-                    <div class="col-md-2">
-                        <input type="text" class="form-control" id="fecha" readonly="true" value = <?= "'".Yii::$app->formatter->asDate($model->PO_FEC, 'php:d-m-Y')."'" ?>>
+                    <div class="col-md-6">
+                        <div class="form-group field-fecha required">
+                            <label class="control-label col-md-4" for="fecha">Fecha</label>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control" id="fecha" readonly="true" value = <?= "'".Yii::$app->formatter->asDate($model->PO_FEC, 'php:d-m-Y')."'" ?>>
+                                <div class="help-block help-block-error "></div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <br>
                 <div class="row">
                     <div class="col-md-6"> 
                         <?= $form->field($model, 'PO_HORA', ['horizontalCssClasses' => ['label' => 'col-md-4', 'wrapper' => 'col-md-4']])->textInput(['readonly' => true,'maxlength' => true]) ?>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group field-muestra required">
+                            <label class="control-label col-md-4" for="muestra">Muestra</label>
+                            <div class="col-md-4">
+                                 <input type="text" class="form-control" id="muestra" readonly="true" value = <?= "'".$model->PO_MUESTRA==1?"Si":"No" ?>>
+                                <div class="help-block help-block-error "></div>
 
-         <!--       <div class="row">
-                    <div class="col-md-6"> 
-                        <?= $form->field($model, 'PO_COLEST', ['horizontalCssClasses' => ['label' => 'col-md-4', 'wrapper' => 'col-md-4']])->textInput(['maxlength' => true, 'readonly' => true]) ?>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
-                <div class="row">
-                    <div class="col-md-6"> 
-                <?= $form->field($model, 'PO_GLUCOSA', ['horizontalCssClasses' => ['label' => 'col-md-4', 'wrapper' => 'col-md-4']])->textInput(['maxlength' => true, 'readonly' => true]) ?>
-                    </div>
-                </div>-->
-                <div class="row">
-                    <div class="col-md-2">
-                        <?='<label class=" control-label col-md-4" for="muestra">Muestra</label>' ?>
-                    </div>
-                    <div class="col-md-2">
-                        <input type="text" class="form-control" id="muestra" readonly="true" value = <?= "'".$model->PO_MUESTRA==1?"Si":"No" ?>>
-                    </div>
-                    
-                </div>
+                
 
                <div class="form-group  pull-right">
        <!-- <h4> Confirma la extracción y toma de muestra? </h4>-->
