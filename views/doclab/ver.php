0b9f8d0e997d750c429fdc4d79a57891d8f92832
VirginiaCardoso <virgicardoso@gmail.com>
2017-01-23 10:37:08 -0300

    ver

---
+++ views/doclab/ver.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model documento_salud\models\Doclab */
+
+
+$this->title = 'Ver Documento Laboral: '.$model->DO_NRO;
+$this->params['breadcrumbs'][] = ['label' => 'Documento Salud Laboral', 'url' => ['/libretas/consulta-medica/']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="doclab-create">
+
+   
+
+    <?= $this->render('_form', [
+        'model' => $model,
+        'lib'=> $lib,
+        'client' =>$client,
+        'docaux' => $docaux,
+    ]) ?>
+
+</div>
