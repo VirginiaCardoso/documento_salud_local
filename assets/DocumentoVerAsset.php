5d9f6c03dfaa0bfc5115cfdacf8ec1d1e4802a1f
VirginiaCardoso <virgicardoso@gmail.com>
2017-02-12 01:41:28 -0300

    assets

---
+++ assets/DocumentoVerAsset.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+
+namespace  documento_salud\assets;
+
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class Documento2Asset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+        'css/libretas.css',
+       // 'css/eventos.css',
+    ];
+    public $js = [
+        'js/bootstrap-toggle.min.js',
+        'js/jquery.mask.js',
+       // 'js/libretas.js',
+     //   'js/modal-cliente.js',
+      // 'js/documento.js',
+       'js/docview.js',
+   
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
---
+++ assets/EmisionAsset.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace  documento_salud\assets;
+
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class EmisionAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+        'css/libretas.css',
+       
+    ];
+    public $js = [
+        'js/bootstrap-toggle.min.js',
+        'js/jquery.mask.js', 
+        'js/emision.js',
+       ];
+
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
