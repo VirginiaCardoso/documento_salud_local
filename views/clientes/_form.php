27fb93f6098c05901411e8957a0f5ab8b74b9842
VirginiaCardoso <virgicardoso@gmail.com>
2017-02-13 10:40:03 -0300

    atributo error date

--- views/clientes/_form.php
+++ views/clientes/_form.php
@@ -64,8 +64,9 @@ Pjax::begin();
 
 <div class="row">
         <div class="col-md-6">
-
-      <?= $form->field($model, 'CL_FECNAC',['addAriaAttributes' => false, 'horizontalCssClasses' => ['label' => 'col-md-4', 'wrapper' => 'col-md-6']])->widget(DateControl::classname(), [
+        <!-- para local -->
+        <!-- 'addAriaAttributes' => false, -->
+      <?= $form->field($model, 'CL_FECNAC',[ 'horizontalCssClasses' => ['label' => 'col-md-4', 'wrapper' => 'col-md-6']])->widget(DateControl::classname(), [
                 'type'=>DateControl::FORMAT_DATE,
                 'ajaxConversion'=>false,
                 'displayFormat' => 'dd/MM/yyyy',
