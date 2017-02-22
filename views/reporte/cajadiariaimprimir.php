e827a71fc843511e99a644d411a48e387daa339a
VirginiaCardoso <virgicardoso@gmail.com>
2017-02-22 14:15:42 -0300

    imprimir reporte caja diaria

---
+++ views/reporte/cajadiariaimprimir.php
@@ -0,0 +1,130 @@
+<?php
+
+use yii\helpers\Html;
+use kartik\grid\GridView;
+use yii\widgets\Pjax;
+use kartik\dialog\Dialog;
+use yii\bootstrap\ActiveForm;
+use kartik\select2\Select2;
+use kartik\datecontrol\DateControl;
+use kartik\daterange\DateRangePicker;
+use documento_salud\models\CajaDiaraFiltro;
+use documento_salud\assets\CajaDiariaAsset;
+
+/* @var $this yii\web\View */
+/* @var $searchModel archivos_hc\models\PedidosFiltro */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+CajaDiariaAsset::register($this);
+
+$this->title = 'Caja Diaria';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="cajadiaria-index">
+    <table class="encabezado" style="width:100%" border="0">
+        <tr >
+            <td> 
+                <?= Html::img('images/logo-medicinaprev.jpg', $options = ['width'=>'230'] );?>
+            </td>
+            <td>
+                <center>
+                    <h4> AÑO <?= date('Y') ?></h4>
+                    <br>
+                   <h4> PLANILLA DE RENDICION <?= Html::encode(date("d-m-Y",strtotime($searchModel->dia))) ?></h4>
+                   <h6> Fecha de Emisión <?= date('d-m-Y') ?></h6>
+               </center>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                
+            </td>
+            <td>
+                <table class="textocajero"  border="0" align="right" >
+                    <tr >
+                        <td align="right" >
+                              <?= Html::img('images/firmacajero.jpg', $options = ['width'=>'330'] );?>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+
+<?php  Pjax::begin(); 
+
+ ?>
+
+<?= GridView::widget([
+        'dataProvider' => $dataProvider,
+       // 'filterModel' => $searchModel,
+                     
+        'columns' => [
+            [
+                'label' => 'Anulada',
+                'value'=> function($model) {
+                    if ($model->LI_ANULADA==0)
+                        return "NO";
+                    else
+                        return "SI";
+                },
+            ],
+            'LI_NRO',
+            'LI_FECPED:date',
+            'LI_HORA',
+            [
+                'label' => 'Cliente',
+                'value'=> function($model) {
+                    if ($model->lICOCLI!=null)
+                        return $model->lICOCLI->CL_APENOM;
+                    else
+                        return "";
+                },
+            ],
+           [
+                'label' => 'Tipo de trámite',
+                'value'=> function($model) {
+                    if ($model->lITPOSER!=null)
+                        return $model->lITPOSER->TS_DESC;
+                    else
+                        return "";
+                },
+            ],
+             [
+                'label' => 'Importe',
+                'value'=> function($model) {
+                    if ($model->LI_IMPORTE!=null) {
+                       // $subimporte = $subimporte + $model->LI_IMPORTE;
+                        return $model->LI_IMPORTE;
+                    }
+                    else
+                        return "0.00";
+                },
+            ],           
+             [
+                'label' => 'Devoluciones',
+                'value'=> function($model) {
+                    if ($model->devolu!=null) {
+                      //  $subdevol = $subdevol + $model->devolu->DE_IMPORT;
+                        return $model->devolu->DE_IMPORT;
+                    }
+                    else
+                        return "0.00";
+                },
+            ],
+        ],
+    ]); ?>
+
+<?php Pjax::end();  
+
+$subvalores = $searchModel->calcularImporte($searchModel->dia);
+echo "<b>Sub- Totales </b><br>";
+echo "Importes: ".$subvalores['subimporte']." $ <br>";
+echo "Devoluciones: ".$subvalores['subdevol']." $ <br>";
+echo "<br>";
+$resta = floatval($subvalores['subimporte'])- floatval($subvalores['subdevol']);
+echo "<b> Total: ".$resta." $<b>";
+
+?>
+
+</div>
