be38bfb057206bdb3d13f6c7e59bfed11ee69e48
VirginiaCardoso <virgicardoso@gmail.com>
2017-03-08 17:56:52 -0300

    restas devoluciones

--- views/reporte/resumenrecaudacion.php
+++ views/reporte/resumenrecaudacion.php
@@ -185,7 +185,7 @@ HTML;
             <table style="width:100%" border="0">
                 <tr>
                     <td>
-                        <b> $ <?= $nuevas_part['recau']+$nuevas_conv['recau']; ?> </b>
+                        <b> $ <?= $nuevas_part['recau']+$nuevas_conv['recau']-$nuevas_part['dev']-$nuevas_conv['dev']; ?> </b>
                     </td>
                 </tr>
                 <tr>
@@ -283,7 +283,7 @@ HTML;
             <table style="width:100%" border="0">
                 <tr>
                     <td>
-                        <b> $ <?= $renov_part['recau']+$renov_conv['recau']; ?> </b>
+                        <b> $ <?= $renov_part['recau']+$renov_conv['recau']-$renov_part['dev']-$renov_conv['dev']; ?> </b>
                     </td>
                 </tr>
                 <tr>
@@ -381,7 +381,7 @@ HTML;
             <table style="width:100%" border="0">
                 <tr>
                     <td>
-                        <b> $ <?= $vencidas_part['recau']+$vencidas_conv['recau']; ?> </b>
+                        <b> $ <?= $vencidas_part['recau']+$vencidas_conv['recau']-$vencidas_part['dev']-$vencidas_conv['dev']; ?> </b>
                     </td>
                 </tr>
                 <tr>
@@ -417,7 +417,7 @@ HTML;
             <table style="width:100%" border="0">
                 <tr>
                     <td>
-                        <b> $ <?= $nuevas_part['recau']+$renov_part['recau']+$vencidas_part['recau']; ?> </b>
+                        <b> $ <?= $nuevas_part['recau']+$renov_part['recau']+$vencidas_part['recau']-$nuevas_part['dev']-$renov_part['dev']-$vencidas_part['dev']; ?> </b>
                     </td>
                 </tr>
                 <tr>
@@ -436,7 +436,7 @@ HTML;
             <table style="width:100%" border="0">
                 <tr>
                     <td>
-                        <b> $ <?= $nuevas_conv['recau']+$renov_conv['recau']+$vencidas_conv['recau']; ?> </b>
+                        <b> $ <?= $nuevas_conv['recau']+$renov_conv['recau']+$vencidas_conv['recau']-$nuevas_conv['dev']-$renov_conv['dev']-$vencidas_conv['dev']; ?> </b>
                     </td>
                 </tr>
                 <tr>
